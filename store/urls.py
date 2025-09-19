fe1c3257eda0be36b5e85d5b0a575503e9444122
philiyonas <philiyonas13@gmail.com>
2025-09-19 20:13:37 +0300

    generic Apiview

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,8 @@ from store import views
 app_name = 'store'
 
 urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:id>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
+    path('products/', views.ProductListCreate.as_view()),
+    path('products/<int:pk>/', views.ProductDetail.as_view()),
+    path('collections/', views.CollectionListCreate.as_view()),
     path('collections/<int:pk>/', views.CollectionDetail.as_view()),
 ]
--- store/views.py
+++ store/views.py
@@ -1,93 +1,54 @@
 # views.py - request handlers for the store app it is convention to name it views.py but it can be named anything
 
 from django.shortcuts import render, get_object_or_404 
-from .models import Product, OrderItem, Collection
+from django.db.models import Count
 from django.http import HttpResponse
+
+from .models import Product, OrderItem, Collection
+from .models import Product,OrderItem, Collection
+from .serializers import ProductSerializer , CollectionSerializer
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status 	
-from .models import Product,OrderItem, Collection
-from .serializers import ProductSerializer , CollectionSerializer
-from django.db.models import Count
-
 from rest_framework.views import APIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView 
 
-class ProductList(APIView):
-	def get(self, request):
-		queryset = Product.objects.all().select_related('collection') # this querry sets all products and their related collection in one go
-		serializer =ProductSerializer(queryset , many=True, # this serializes the querryset to json using the ProductSerializer
-							     context={'request': request}) # include request in context for HyperlinkedRelatedField
-		
-		return Response(serializer.data, status=status.HTTP_200_OK)
-	
-	def post(self, request):
-		deserializer = ProductSerializer(data=request.data) # this deserializes the json data to a python object
-		deserializer.is_valid(raise_exception=True)# check if the deserialized data is valid
-		print(deserializer.validated_data) # access the validated data
-		deserializer.save() # save the new product to the database
-		return Response(deserializer.data, status=status.HTTP_201_CREATED) # return the serialized data with a 201 status code
 
-class ProductDetail(APIView):
-	def get(self, request, id):
-		product = get_object_or_404(Product, pk=id)#retrieve a product by its primary key (id) or return a 404 error if not found
-		serializer = ProductSerializer(product, context={'request': request})# serialize the product to json using the ProductSerializer
-		return Response(serializer.data, status=status.HTTP_200_OK)
+class ProductListCreate(ListCreateAPIView):
+	queryset = Product.objects.all().select_related('collection') # this querry sets all products and their related collection in one go
+	serializer_class = ProductSerializer
 	
-	def put(self, request, id):
-		product = get_object_or_404(Product, pk=id)
-		deserializer = ProductSerializer(product, data=request.data) # this deserializes the json data to a python object
-		deserializer.is_valid(raise_exception=True)# check if the deserialized data is valid
-		deserializer.save() # save the updated product to the database
-		return Response(deserializer.data, status=status.HTTP_200_OK) # return the serialized data with a 200 status code
+	def get_serializer_context(self):
+		return {'request': self.request} # include request in context for HyperlinkedRelatedField
+ 	 		
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+	queryset = Product.objects.all() # this querry sets all products and their related collection in one go
+	serializer_class = ProductSerializer # serialize the product to json using the ProductSerializer
 	
-	def patch(self, request, id):
-		product = get_object_or_404(Product, pk=id)
-		deserializer = ProductSerializer(product, data=request.data, partial=True) # this deserializes the json data to a python object
-		deserializer.is_valid(raise_exception=True)# check if the deserialized data is valid
-		deserializer.save() # save the updated product to the database
-		return Response(deserializer.data, status=status.HTTP_200_OK) # return the serialized data with a 200 status code
+	def get_serializer_context(self):
+		return {'request': self.request} # include request in context for HyperlinkedRelatedField
 	
-	def delete(self, request, id):
-		product = get_object_or_404(Product, pk=id)
-		# check for related OrderItem rows before deleting
+	def delete(self, request, pk):
+		product = get_object_or_404(Product, pk=pk)
 		if product.orderitems.count() > 0:
 			return Response({'error':'product cannot be deleted b/c it has an order'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 		product.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT) # return a 204 status code indicating
 		# that the product was successfully deleted and there is no content to return
 
-class CollectionList(APIView):
-	def get(self, request):
-		collection = Collection.objects.annotate(products_count=Count('products')) # annotate each collection with the count of related products
-		serializer = CollectionSerializer(collection, many=True, context={'request': request}) # serialize the collection to json using the CollectionSerializer
-		return Response(serializer.data, status=status.HTTP_200_OK)
-	
-	def post(self, request):
-		deserializer = CollectionSerializer(data=request.data) # this deserializes the json data to a python object
-		deserializer.is_valid(raise_exception=True)# check if the deserialized data is valid
-		deserializer.save() # save the new collection to the database
-		return Response(deserializer.data, status=status.HTTP_201_CREATED) # return the serialized data with a 201 status code
+class CollectionListCreate(ListCreateAPIView):
+	queryset = Collection.objects.annotate(products_count=Count('products')) # annotate each collection with the count of related products
+	serializer_class = CollectionSerializer
 
-class CollectionDetail(APIView):
-	def get(self, request, pk):
-		collection = get_object_or_404(Collection, pk=pk)
-		serializer = CollectionSerializer(collection, context={'request': request}) # serialize the collection to json using the CollectionSerializer
-		return Response(serializer.data, status=status.HTTP_200_OK)
-	
-	def put(self, request, pk):
-		collection = get_object_or_404(Collection, pk=pk)
-		deserializer = CollectionSerializer(collection, data=request.data) # this deserializes the json data to a python object
-		deserializer.is_valid(raise_exception=True)# check if the deserialized data is valid
-		deserializer.save() # save the updated collection to the database
-		return Response(deserializer.data, status=status.HTTP_200_OK) # return the serialized data with a 200 status code
-	
-	def patch(self, request, pk):
-		collection = get_object_or_404(Collection, pk=pk)
-		deserializer = CollectionSerializer(collection, data=request.data, partial=True) # this deserializes the json data to a python object
-		deserializer.is_valid(raise_exception=True)# check if the deserialized data is valid
-		deserializer.save() # save the updated collection to the database
-		return Response(deserializer.data, status=status.HTTP_200_OK) # return the serialized data with a 200 status code
-	
+	def get_serializer_context(self):
+		return {'request': self.request} # include request in context for HyperlinkedRelatedField
+
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+	queryset = Collection.objects.all() # this querry sets all products and their related collection in one go
+	serializer_class = CollectionSerializer
+
 	def delete(self, request, pk):
 		collection = get_object_or_404(Collection, pk=pk)
 		if Product.objects.filter(collection=collection).exists():
